2a08e01c30ef7b8c1fbe7973f8000ba6ed58e23f
Nikolay Bortsov <nickob132@gmail.com>
2023-01-26 23:01:08 +0000

    Set up minimal project

---
+++ src/knn.cpp
@@ -0,0 +1,10 @@
+#include <knn.h>
+
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+  std::cout << "Hello World!" << std::endl;
+  return EXIT_SUCCESS;
+}
---
+++ src/knn.h
@@ -0,0 +1,12 @@
+#ifndef KNN_H
+#define KNN_h
+
+// Load the traing data
+
+// Save training data to a file
+
+// Calculate training data
+
+// Predict
+
+#endif
